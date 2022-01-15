8c9387fcff9273064000e5bb5e73113d30520956
Rafael Basterra Careaga <rbasterra@msn.com>
2022-01-15 10:27:22 +0100

    add delete button functionality

--- main.js
+++ main.js
@@ -26,14 +26,47 @@ addBtn.addEventListener('click',(e)=>{
         //asignamos el texto al parrafo
         p.textContent = text;
 
-        //Añadimos p como hijo de li y a li hijo de la lista
+        //Añadimos p como hijo de li y a li hijo de la lista. Añadimos tb el boton de borrar elemento
         li.appendChild(p);
+        li.appendChild(addDeleteBtn());
         ul.appendChild(li);
 
         //resetear el valor del input para que desaparezca lo que hemos escrito
         input.value = '';
 
         //ocultar el mensaje de abajo
-        empty.styles.display=none;
+        empty.style.display= 'none';
     }
-})
+});
+
+function addDeleteBtn(){
+    //crear el boton de borrar el elemento
+    const deleteBtn = document.createElement('button');
+
+    //Añadir el texto del boton
+    deleteBtn.textContent = 'X';
+
+    //Añadir la clase de estilo
+    deleteBtn.className = 'btn-delete';
+
+    //Agregar un eventListener para borrar el elemento de la lista
+    deleteBtn.addEventListener('click', (e)=>{
+        //Seleccionamos el li que vamos a eliminar. Esa informacion viene en el propio evento que recibimos como parametro
+        const item = e.target.parentElement;
+
+        // eliminamos el elemento de la lista. Tambien se puede hacer directamente en el evento (e.target.parentElement.remove()) o con item.remove()
+        ul.removeChild(item);
+
+        //Una vez borrado el elemento, tenemos que volver a incluir el mensaje de empty
+        //Para ello, podemos seleccionar todos los li's que quedan en el documento. Si tuviesemos mas li's en el documento, lo suyo seria añadirles una clase al li para poder seleccionarlo luego 
+        //Por ejemplo, li.classList.add('liClass')
+        const items = document.querySelectorAll('li');
+
+        //si no hay items en la lista, volvemos a mostrar empty
+        if (items.length === 0){
+            empty.style.display = 'block';
+        }
+    })
+
+    return deleteBtn;
+}
